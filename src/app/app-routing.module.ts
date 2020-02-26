959d84f9f7dbc102563410c6aaee272994c211c6
bey med habib <beymedhabib@gmail.com>
2020-02-26 18:01:18 +0100

    up

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AddComponent } from './add/add.component';
+import { ListComponent } from './list/list.component';
+import { UpdateComponent } from './update/update.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { WeatherComponent } from './weather/weather.component';
+import { LoginComponent } from './login/login.component';
+import { RegistreComponent } from './registre/registre.component';
+import { GuardGuard } from './guard.guard';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'add', component: AddComponent, canActivate: [GuardGuard]},
+  { path: 'list', component: ListComponent, canActivate: [GuardGuard]},
+  { path: 'update/:i', component: UpdateComponent, canActivate: [GuardGuard]},
+  { path: 'weather' , component: WeatherComponent, canActivate: [GuardGuard]},
+  { path: 'login' , component: LoginComponent},
+  { path: 'registre' , component: RegistreComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,55 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule} from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddComponent } from './add/add.component';
+import { ListComponent } from './list/list.component';
+import { UpdateComponent } from './update/update.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Routes, RouterModule } from '@angular/router';
+import { MaterialModule } from './material/material.module';
+import { WeatherComponent } from './weather/weather.component';
+import { LoginComponent } from './login/login.component';
+import { RegistreComponent } from './registre/registre.component';
+import { PipePipe } from './pipe.pipe';
+import { CheckPipe } from './check.pipe';
+
+// const routes: Routes = [
+//   {path:  '', pathMatch:  'full', redirectTo:  'home'},
+//   {path: 'navbar', component: NavbarComponent},
+//   {path: 'add', component: AddComponent},
+//   {path: 'list', component: ListComponent}
+// ];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddComponent,
+    ListComponent,
+    UpdateComponent,
+    NavbarComponent,
+    WeatherComponent,
+    LoginComponent,
+    RegistreComponent,
+    PipePipe,
+    CheckPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MaterialModule,
   ],
+  exports : [RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+ MDBBootstrapModule;
+}
